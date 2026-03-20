20ae7be318ddb1c206539e2e3f2010e45079c43d
Srinidhicce <srinidhi.ps2022cce@sece.ac.in>
2026-03-20 10:30:21 +0530

    add vercel url to cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,9 +33,12 @@ app.use(helmet({
     }
   }
 }));
-
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:5173', 'http://localhost:3000'],
+  origin: process.env.FRONTEND_URL || [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://errorloganalyzer.vercel.app'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
